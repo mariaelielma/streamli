48e67e1471547d23bf96a841da21f7c304f1aa9d
mariaelielma <elielmafarias2004@gmail.com>
2026-07-30 11:24:31 -0300

    Integrate Streamlit for community data visualization
    Added Streamlit integration to visualize quilombola communities data.

--- UFPB.py
+++ UFPB.py
@@ -60,3 +60,30 @@ st.write('Você selecionou: ', opcao)
 #filtrando os dados pelo nome
 dadosFiltrados = df[df['nomeServidor'] == opcao]
 dadosFiltrados
+
+import streamlit as st
+import pandas as pd
+#import no requirements.txt
+
+st.title('Localização das comunidades quilombolas (2022)')
+
+#preparando o dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
+#apresentar o dataframe
+#https://www.ibge.gov.br/geociencias/organizacao-do-territorio/estrutura-territorial/27385-localidades.html?=&t=downloads
+#apresentar o dicionario de dados (disponível em Localizado em "Downloads > Localidades quilombolas > Arquivos vetoriais > dicionario LQs")
+#informar que dados são remanunfaturados (vamos aproveitar dados de pesquisas já em andamento)
+
+#informar que precisa limpar os dados
+df.info()
+
+#criando um slider
+numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
+st.write(df.head(numero))
+
+#visualizando o número de comunidades por estado
+df['NM_UF'].value_counts()
+
+#criando um gráfico de barras
+st.bar_chart(df['NM_UF'].value_counts())
