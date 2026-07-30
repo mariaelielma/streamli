33b089cf2e81bd6f419427c3e074bb6a587b31b8
mariaelielma <elielmafarias2004@gmail.com>
2026-07-30 11:23:01 -0300

    Implement basic Streamlit app with server selection
    Added a simple Streamlit application to display a table and select a server.

--- UFPB.py
+++ UFPB.py
@@ -33,3 +33,30 @@ st.success("Você conseguiu!")
 st.error("Erro!")
 st.warning("Advertência")
 st.info("Esta é uma informação")
+
+#criando uma aplicação simples
+import streamlit as st
+#import no requirements.txt
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+#posso trocar estes dados por dados de tabelas reais!
+
+st.write("Criando uma tabela!")
+#tabelas interativas
+st.write(df)
+#inserindo um selectbox
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+#O formato de print é diferente de outras versões
+#de Python
+st.write('Você selecionou: ', opcao)
+#como filtrar os dados pelo nome?
+
+#filtrando os dados pelo nome
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+dadosFiltrados
